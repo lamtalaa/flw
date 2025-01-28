791794d9588fa60e31babe58a12545d8a05ed439
Yassine Lamtalaa <ysnlmtl3@gmail.com>
2025-01-28 12:57:52 -0500

    modify gifs

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ const gif = document.querySelector(".gif");
 
 // Change text and gif when the Yes button is clicked
 yesBtn.addEventListener("click", () => {
-    question.innerHTML = "Being with you is my biggest blessing. I love you.";
-    gif.src = "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExbGNhdXh1b252b2F2b2U4cHRlNGkwMDZsajllaGF1cDJyb2p4NXl2YiZlcD12MV9naWZzX3NlYXJjaCZjdD1n/G6N0pDDgDpLjUvNoyQ/giphy.gif";
+    question.innerHTML = "Me too.";
+    gif.src = "https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExdTB5Mm5tczVsY2hvaDJpY2JkMXUwbTN0azJlbG56MHU0a2VmeWdqdCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3o6gE0pyow6DYSDo52/giphy.gif";
 
     // Hide the No button
     noBtn.style.display = "none";
